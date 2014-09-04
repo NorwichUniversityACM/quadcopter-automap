68b1a763192de9bc5d4436cff8658a609d94f9f3
BenJJones <patsfan160@gmail.com>
2014-09-04 19:58:37 -0400

    New Versions

--- client/main.py
+++ client/main.py
@@ -1,12 +1,13 @@
 import socket
 import Queue
+import threading
 
 TCP_IP = '10.0.0.1'
 TCP_PORT = 666
 BUFFER_SIZE = 1024
 #Constants
 
-socket = None
+s = None
 data_queue = Queue.Queue()
 
 receiving = True
@@ -16,56 +17,55 @@ active = True
 send_per = 0
 
 def connect(ip, port):
-	socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	socket.connect((ip, port))
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, port))
 def close():
-	socket.close()
+        s.close()
 def send(message):
-	socket.send(message)
+        s.send(message)
 def receive():
-	data_queue.put(socket.recv(BUFFER_SIZE))
+        data_queue.put(s.recv(BUFFER_SIZE))
 def receive_loop():
-	while receiving:
-		receive()
-		time.sleep(0.5)
+        while receiving:
+                receive()
+                time.sleep(0.5)
 def send_loop():
-	while sending:
-		send(str(send_per))
-		time.sleep(.01)
+        while sending:
+                send(str(send_per))
+                time.sleep(.01)
 def print_received():
-	while active:
-		while not data_queue.empty():
-			print data_queue.get()
-	time.sleep(.01)
+        while active:
+                while not data_queue.empty():
+                        print data_queue.get()
+        time.sleep(.01)
 if __name__ == "__main__":
-	try:
-		connect(TCP_IP, TCP_PORT)
+        
+        connect(TCP_IP, TCP_PORT)
+        sending = True
+        receiving = True
+        active = True   
+                  
+        rec_d = threading.Thread(target=receive_loop, args = ())
+        rec_d.daemon = True
+        rec_d.start()   
+        send_d = threading.Thread(target=send_loop, args = ())
+        send_d.daemon = True
+        send_d.start()
+        print_d = threading.Thread(target=print_received, args = ())
+        print_d.daemon = True
+        print_d.start()
+                
+        while send_per >= 0:
+                send_per = input("#:")
+        
+        sending = False
+        receiving = False
+        active = False
 
-		sending = True
-		receiving = True
-		active = True	
-		
-		rec_d = threading.Thread(target=receive_loop, args = ())
-   		rec_d.daemon = True
-  	  	rec_d.start()	
-		send_d = threading.Thread(target=send_loop, args = ())
-    		send_d.daemon = True
-    		send_d.start()
-	 	print_d = threading.Thread(target=print_received, args = ())
-                print_d.daemon = True
-                print_d.start()
-		
-		while send_per >= 0:
-			send_per = input("#:")
-	
-		sending = False
-		receiving = False
-		active = False
-
-		rec_d.join()
-		send_d.join()
-		print_d.join()
-		close()
-	except:
-		print "Failure...Connection closed/timeout/etc"			
+        rec_d.join()
+        send_d.join()
+        print_d.join()
+        close()
+        
+        print "Failure...Connection closed/timeout/etc"                 
 
