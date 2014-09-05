cbf16d4cd0f79a2a2a8ce2c878bac55845c19576
Joshua Hartley <jhartle1@stu.norwich.edu>
2014-09-05 08:36:09 -0400

    Added HardwareBase class for managing the Quad hardware. Written to spec of the whiteboard. Sonar and Gyro are not implemented

---
+++ server/HardwareBase.py
@@ -0,0 +1,39 @@
+import Adafruit_BBIO.PWM as PWM
+
+class HardwareBase:
+	MOTORS = {0: "P9_14", 1:"P8_13", 2:"P9_21", 3:"P9_42"}
+	FREQUENCY = 300.0
+	PERIOD = 1.0 / FREQUENCY
+	MINIMUM_DUTY_CYCLE = 100.0 * (.001 / PERIOD)
+	MAXIMUM_DUTY_CYCLE = 100.0 * (.002 / PERIOD)
+	
+	def __init__(self):
+		self.curr_motor_speed = {0:0,1:0,2:0,3:0}	
+	def start():
+		for motor in HardwareBase.MOTORS:
+			PWM.start(HardwareBase.MOTORS[motor],MINIMUM_DUTY_CYCLE, HardwareBase.FREQUENCY)
+
+	def stop():
+		for motor in HardwareBase.MOTORS:
+			PWM.stop(HardwareBase.MOTORS[motor])
+	
+	def percent_to_duty(percent):
+		return ((0.01*percent) * (HardwareBase.MAXIMUM_DUTY_CYCLE - HardwareBase.MINIMUM_DUTY_CYCLE)) + HardwareBase.MINIMUM_DUTY_CYCLE
+	def getMotorSpeed(motor_num):
+		return self.curr_motor_speed[motor_num]
+	def setMotorSpeed(motor_num, float percent):
+		if percent >= 100:
+			percent = 99
+		else if percent < 0:
+			percent = 0
+		self.curr_motor_speed[motor_num] = percent
+		PWM.set_duty_cycle(HardwareBase.MOTORS[motor_num], self.percent_to_duty(percent))
+	def getSonarDistance(sonar_num)
+		return 0
+	def getPitch()
+		return 0
+	def getRoll()
+		return 0
+	def getYaw()
+		return 0
+
